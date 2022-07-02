4c6c08b5d1fe2fff5c1721eb11a626dddf5aaddb
Alexander <al.x.falconer@gmail.com>
2022-07-02 12:03:40 -0400

    solution

--- curriculum/02-stacks/balancedPairings.js
+++ curriculum/02-stacks/balancedPairings.js
@@ -1,5 +1,25 @@
 // write an algorithm that will read a string of parentheses from left to right and decide whether the symbols are balanced
 
+function balancedParentheses(str) {
+	let stack = [];
+	const OPENING = '(';
+	const CLOSING = ')';
+
+	for(let char of str){
+		if(char === OPENING) {
+			stack.push(char);
+		} else if(char === CLOSING) {
+			const removedChar = stack.pop();
+
+			if(typeof removedChar === 'undefined'){
+				return false;
+			}
+		}
+	}
+
+	return (stack.length === 0);
+}
+
 // console.log(balancedParentheses('(()()()())')); // true
 // console.log(balancedParentheses('((((((())'));  // false
 // console.log(balancedParentheses('((((((())'));  // false
